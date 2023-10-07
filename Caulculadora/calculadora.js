fa5d5f16478a6e81ac073382333613c9c7c337fe
Cauezinho <euacfernandes@gmail.com>
2023-10-07 02:41:38 -0300

    Gerador aleatório e Início de Revisão de Fundamentos

--- Caulculadora/calculadora.js
+++ Caulculadora/calculadora.js
@@ -65,3 +65,58 @@ function exponenciar(){
     console.log("Resultado: "+ resultado);
     document.getElementById("resposta").textContent = resultado;
 }
+
+function fatorial() {
+    console.log(" ")
+    console.log("Fatorial: ")
+    var x = Number(document.getElementById("X").value);
+    while(x>0){
+    var progressao = x-1;
+    var y = x * (progressao);
+    };
+    // y=x; y=y-1; fatorial=x*y; 
+    var resultado = y;
+    console.log("Resultado: " + resultado);
+    document.getElementById("resposta").textContent = resultado;
+
+}
+// otras gracinhas:
+// Calculadora logarítma...
+
+
+//Frases do dia de curiosidades matemáticas
+
+
+function selecionar(min,max){
+
+    var frase1 = "Sabia que (1+1/12)^12 > (1+1/6)^6 > (1+1/2)^2? Essa é a constante de Euler.";
+    var frase2 = "O número PI é o resultado de uma circunferência dividida pelo seu diâmetro.";
+    var frase3 = "Estou cansado agora, tente novamente mais tarde ;)"
+
+    min = Math.ceil(1);
+    max = Math.floor(3);
+    
+    var frase =  Math.floor(Math.random() * (3 - 1 + 1)) + min;
+
+    // switch(frase){
+    //     case 1:document.getElementById("msgdodia").textContent= frase1;
+    //     break;
+    //     case 2:document.getElementById("msgdodia").textContent= frase2;
+    //     break;
+    //     case 3:document.getElementById("msgdodia").textContent= frase3;
+    //     break;
+    // };
+
+    console.log(frase);
+    if(frase==1){
+        document.getElementById("msgdodia").textContent= frase1;
+    };
+    if(frase==2){
+        document.getElementById("msgdodia").textContent= frase2;
+    };
+    if(frase==3){
+        document.getElementById("msgdodia").textContent= frase3;
+    };
+    
+};
+
---
+++ Fundamentos/html.js
@@ -0,0 +1,66 @@
+/*
+
+-> HTML Estrutura Básica:
+
+(Digite "!" + Enter, Editor de código apresentará:)
+
+<!DOCTYPE html>
+// Declaração da linguagem;
+
+<html lang = "en">
+// ou "pt-br"
+
+<head>
+    <meta charset = "UTF-8">
+    // define página de códigos do documento
+    
+    <meta name = "viewport" content = "width=device-width, initial-scale=1.0">
+    // Metatag relacionada à criação de páginas responsivas
+    // respondendo adequadamente aos diversos dispositivos utilizados pelo usuário
+    
+    <meta http-equiv="X-UA-Compatible" content="ie-edge">
+    // Compatibiliadade entre diversos navegadores
+    
+    <title> Nome da Página </title>
+
+</head>
+
+<body>
+</body>
+    <h1></h1> // Título 1
+    <h2...
+    <p> Parágrafo </p>
+    <p> Nome: <input type= "text" id="nome"> 
+    //Campo de entrada de dados do tipo texto
+
+    <input type="button" value="Mostrar">
+    //Botão com nome "mostrar"
+
+    </p>
+    
+    <p id="resposta"> </p> 
+    // inserção de variável através do id do parágrafo 
+
+</html>
+
+-> JavaScript:
+
+Declaração de função:
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+*/
